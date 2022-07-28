26cd8efeacc92c12f45281815301ba207d49e34b
Marcelo <ifrnjoao@gmail.com>
2022-07-27 21:31:55 -0300

    att

--- LojaVirtual/LojaVirtual/settings.py
+++ LojaVirtual/LojaVirtual/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LojaVirtual.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
